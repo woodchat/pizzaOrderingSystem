8e7053ba6e1876f76fce7a9c4a74b46ebb3c8b76
Shubhendu Shishir <shubhendu@Shubhendus-MacBook-Pro.local>
2020-06-09 00:15:12 +0530

    Creating a spring boot project for ordering pizza

---
+++ src/main/java/com/aiden/pizzaOrderingSystem/pizzaOrderingSystem/PizzaOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aiden.pizzaOrderingSystem.pizzaOrderingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiden/pizzaOrderingSystem/pizzaOrderingSystem/PizzaOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiden.pizzaOrderingSystem.pizzaOrderingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
